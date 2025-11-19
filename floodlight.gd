009ce96434dc997bcd997405a4ad094bfb546a91
peter-tres <motap95@gmail.com>
2025-11-19 09:35:36 -0800

    floodlights added, msaa turned on, world environment moved out of map scene

---
+++ floodlight.gd
@@ -0,0 +1,15 @@
+class_name Floodlight extends PoweredLight 
+
+
+
+
+func handle_generator_turned_off() -> void:
+	light.hide()
+	light_mesh.hide()
+	pass
+
+
+func handle_generator_turned_on() -> void:
+	light.show()
+	light_mesh.show()
+	pass
