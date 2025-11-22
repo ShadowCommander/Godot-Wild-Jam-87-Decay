4872d92078c7c4948f71a1d309e71cfa60340028
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2025-11-22 05:08:54 -0800

    Add pickup and drop animations

--- Entities/InteractableObjects/dispenser_component.gd
+++ Entities/InteractableObjects/dispenser_component.gd
@@ -1,6 +1,7 @@
 class_name DispenserComponent extends Component
 
 @export var dispensed_scene: PackedScene
+@export var spawn_position: Node3D
 
 func connect_signals_on_parent() -> void:
 	entity.connect("pressed", handle_pressed)
@@ -14,5 +15,9 @@ func handle_pressed(_event: InteractionSystem.InteractionData) -> void:
 	if not can_pickup_event.can_pickup:
 		return
 	var pickup_event: HandSystem.HandPickupEvent = HandSystem.HandPickupEvent.new()
-	pickup_event.item = dispensed_scene.instantiate()	
+	var item: Node3D = dispensed_scene.instantiate()
+	get_tree().root.add_child(item)
+	item.global_position = spawn_position.global_position
+	item.reset_physics_interpolation()
+	pickup_event.item = item
 	user.emit_signal("hand_pickup", pickup_event)
--- Entities/Player/hand_system.gd
+++ Entities/Player/hand_system.gd
@@ -12,8 +12,16 @@ class HandCanDropIntoEvent:
 
 class HandDropIntoEvent:
 	var item: Node3D = null
+	var target: Node3D = null
+
+@export var hand_position: Node3D
 
 var hand: Node3D = null
+
+func _process(delta: float) -> void:
+	process_animation(delta)
+
+#region Component signals
 
 func setup_signals_on_parent() -> void:
 	entity.add_user_signal("hand_can_pickup", [
@@ -35,6 +43,10 @@ func connect_signals_on_parent() -> void:
 	entity.connect("hand_can_drop_into", handle_can_drop_into)
 	entity.connect("hand_drop_into", handle_drop_into)
 
+#endregion
+
+#region Pickup
+
 func handle_can_pickup(event: HandCanPickupEvent) -> void:
 	if hand != null:
 		event.can_pickup = false
@@ -42,8 +54,15 @@ func handle_can_pickup(event: HandCanPickupEvent) -> void:
 func handle_pickup(event: HandPickupEvent) -> void:
 	if event.item == null:
 		return
-	hand = event.item
+	
+	var item = event.item
+	hand = item
+	animate_item_movement(item, hand_position)
 	print("Picked up ", event.item, event.item.name)
+
+#endregion
+
+#region Drop
 
 func handle_can_drop_into(event: HandCanDropIntoEvent) -> void:
 	if hand == null:
@@ -53,5 +72,49 @@ func handle_can_drop_into(event: HandCanDropIntoEvent) -> void:
 
 func handle_drop_into(event: HandDropIntoEvent) -> void:
 	event.item = hand
+	animate_item_movement(hand, event.target, handle_item_dropped.bind(hand))
 	hand = null
+	event.item.emit_signal("dropped")
 	print("Dropped ", event.item, event.item.name)
+
+#endregion
+
+#region Animation
+
+var item_pickup_time: float = 0.2
+var current_time: float = 0
+var start: Vector3
+var end_node: Node3D
+var animating_item: Node3D
+
+var pickup: bool = false
+var call: Callable
+
+func process_animation(delta: float) -> void:
+	if not pickup:
+		return
+	current_time += delta
+	var weight: float = min(current_time / item_pickup_time, 1.0)
+	animating_item.global_position = lerp(start, end_node.global_position, ease(weight, 3))
+	if weight >= 1:
+		pickup = false
+		call.call()
+		current_time = 0
+
+func animate_item_movement(item: Node3D, target: Node3D, callback: Callable = handle_item_picked_up.bind(item)) -> void:
+	start = item.global_position
+	end_node = target
+	pickup = true
+	call = callback
+	animating_item = item
+	
+@onready var root = get_tree().root
+
+func handle_item_picked_up(item: Node3D) -> void:
+	item.reparent(hand_position, false)
+	item.position = Vector3.ZERO
+
+func handle_item_dropped(item: Node3D) -> void:
+	item.reparent(root, true)
+
+#endregion
